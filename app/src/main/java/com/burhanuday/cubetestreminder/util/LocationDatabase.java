73a8198c800abe4df8d35a5af4531055270c45fe
Burhanuday <udaipurwalaburhanuddin@gmail.com>
2018-12-12 13:45:23 +0530

    before major changes

--- app/src/main/java/com/burhanuday/cubetestreminder/util/LocationDatabase.java
+++ app/src/main/java/com/burhanuday/cubetestreminder/util/LocationDatabase.java
@@ -10,7 +10,21 @@ import com.burhanuday.cubetestreminder.model.Location;
 /**
  * Created by burhanuday on 12-12-2018.
  */
-@Database(entities = {Location.class}, version = 1, exportSchema = false)
+@Database(entities = {Location.class}, version = 1)
 public abstract class LocationDatabase extends RoomDatabase {
+
+    private static volatile LocationDatabase INSTANCE;
+
+    public static LocationDatabase getInstance(Context context){
+        if (INSTANCE == null){
+            synchronized (LocationDatabase.class){
+                if (INSTANCE == null){
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            LocationDatabase.class, "locations").build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
     public abstract LocationDao locationDao();
 }
